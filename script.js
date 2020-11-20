f4da47b54e619ee4211ff89cc63d5b592daa3cf7
Yasir <goodwonder5@gmail.com>
2020-11-20 21:07:03 +0300

    animated template

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    
+AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    once: false, // whether animation should happen only once - while scrolling down
+});
